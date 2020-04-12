270635bf1f0b7ae1e3637e0a0bc3aefe6c860533
Silas Amarasinghe <aldous.silas@yahoo.com>
2020-04-11 22:48:49 -0400

    Add Code for smooth splines & cubic splines
    1. filtered one country and the created the date differences.
    2. Used date differences as X and confirmed as Y and created splines for only Canada

--- www/scripts/prelim_analysis_silas.R
+++ www/scripts/prelim_analysis_silas.R
@@ -6,7 +6,7 @@
 # r packages used
 rpkgs <- 
   c("dplyr", "stringr", "shiny", "scales", "shinydashboard", "shinythemes", "shinyWidgets", "shinyjs", 
-    "googledrive", "googlesheets4")
+    "googledrive", "googlesheets4", "ISLR", "splines")
 
 # install r packages that have not been installed
 rpkgs_to_install <- 
@@ -30,12 +30,70 @@ scales::show_col(theme_pallete)
 drive_deauth()
 sheets_deauth()
 
-# url to covid19 public google sheet
-gsheets_url <- "https://docs.google.com/spreadsheets/d/1P3PbK_c2V_k2H2xv4FnbpErmE5AjOQV0MC4_BsguFR0/edit?usp=sharing"
+# ====================================== #
+# ------      data from github   -------#
+# ====================================== #
 
-# read covid19 
-daily_num_cases <- 
-  read_sheet(as_id(gsheets_url), sheet = "daily_num_cases")
+covid19_data <- 
+  read.csv(text = RCurl::getURL("https://raw.githubusercontent.com/ashten28/covid19_analysis/master/01_data/covid19_daily_cases_cleaned.csv")) %>% 
+  as_tibble() %>% 
+  mutate(
+    report_date = as.Date(as.character(report_date))
+  )
 
-glimpse(daily_num_cases)
 
+countries_list <-
+  covid19_data %>% 
+  distinct(country_region) %>% 
+  pull() %>% 
+  as.character() %>% 
+  sort()
+
+covid19_data_latest <-
+  covid19_data %>% 
+  filter(report_date == max(report_date))
+
+total_confirmed <- sum(covid19_data_latest$cum_confirmed)
+total_deaths    <- sum(covid19_data_latest$cum_deaths)
+total_recovered <- sum(covid19_data_latest$cum_recovered)
+
+#Choosing a country and a region for prediction
+
+check_out <- covid19_data %>% filter(country_region == "Canada",province_state == "Ontario")
+check_out$first_date <- min(check_out$report_date)
+check_out$day_dff <- as.Date(as.character(check_out$report_date), format="%Y-%m-%d")-
+                          as.Date(as.character(check_out$first_date), format="%Y-%m-%d")
+
+
+check_out$inc_confirmed[38] <- 400
+#cubic spline
+fit<-lm(inc_confirmed ~ bs(day_dff,knots = c(43,50,65)),data = check_out )
+summary(fit)
+
+#plot
+#Plotting the  Line to the scatterplot   
+day_dff_limits<-range(check_out$day_dff)
+#Generating Test Data
+day_dff_grid<-seq(from=day_dff_limits[1], to = day_dff_limits[2])
+
+plot(check_out$day_dff,check_out$inc_confirmed,col="grey",xlab="incremental Days",ylab="Confirmed Cases")
+points(day_dff_grid,predict(fit,newdata = list(day_dff=day_dff_grid)),col="darkgreen",lwd=2,type="l")
+#adding cutpoints
+abline(v=c(43,50,65),lty=2,col="darkgreen")
+
+
+
+#fitting smoothing splines using smooth.spline(X,Y,df=...)
+fit1<-smooth.spline(check_out$day_dff,check_out$inc_confirmed,df=16) #16 degrees of freedom
+
+#Plotting both cubic and Smoothing Splines 
+plot(check_out$day_dff,check_out$inc_confirmed,col="grey",xlab="incremental Days",ylab="Confirmed Cases")
+points(day_dff_grid,predict(fit,newdata = list(day_dff=day_dff_grid)),col="darkgreen",lwd=2,type="l")
+
+#adding cutpoints
+abline(v=c(25,40,60),lty=2,col="darkgreen")
+lines(fit1,col="red",lwd=2)
+legend("topright",c("Smoothing Spline with 16 df","Cubic Spline"),col=c("red","darkgreen"),lwd=2)
+
+
+                                     
